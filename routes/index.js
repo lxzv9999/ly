dc3d0d33fe603ff7e6cee6a406520a56bca245a4
lxzv9999 <1138830257@qq.com>
2017-10-26 14:57:09 +0800

    Signed-off-by: lxzv9999 <1138830257@qq.com>

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var fs=require('fs');
+/* GET home page. */
+router.post('/', function(req, res, next) {
+    // res.readFile("public/wenjian.txt","utf-8",function(err,data){
+    res.header('Access-Control-Allow-Origin','*');
+   fs.readFile("public/wenjian.txt","utf-8",function(err,data){
+       var str=JSON.parse(data);
+       str.push({name:req.body.name});
+       console.log(str);
+       fs.writeFile('public/wenjian.txt',JSON.stringify(str),function(err){
+           fs.readFile("public/wenjian.txt","utf-8",function(err,ccc){
+               var data=JSON.parse(ccc);
+               res.send({name:data})
+           })
+       })
+   })
+});
+ module.exports = router;
